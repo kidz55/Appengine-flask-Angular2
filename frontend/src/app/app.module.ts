8f9ea7565e5af7878c69c63aa7ff5e3ef187e7fd
GLOAGUEN <tgloague@gmail.com>
2017-03-25 13:26:59 +0100

    clean up the code

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,30 +4,15 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { HttpService } from './http.service';
 import {SlimLoadingBarModule} from 'ng2-slim-loading-bar';
-
 import { RouterModule, Routes } from '@angular/router';
-
 import { AppComponent } from './app.component';
-import { GalleryComponent } from './gallery/gallery.component';
-import { RandomComponent } from './random/random.component';
-
 import { ModalModule } from 'angular2-modal';
 import { BootstrapModalModule } from 'angular2-modal/plugins/bootstrap';
 
 
-
-const routes: Routes = [
-  { path: 'random', component: RandomComponent },
-  { path: 'gallery',  component: GalleryComponent },
-  { path: '**', redirectTo: 'gallery' }
-];
-
-
 @NgModule({
   declarations: [
-    AppComponent,
-    GalleryComponent,
-    RandomComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
